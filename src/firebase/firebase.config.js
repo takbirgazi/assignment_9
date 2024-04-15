466318992199a7a3b15918d048bfc8505cd49ffe
takbirgazi <takbirgazibd@gmail.com>
2024-04-15 19:24:49 +0600

    Make a log in Page

---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyC3dN1XRbSh0fRzg696szMFrZQMqmS3MIQ",
+  authDomain: "assignment9-6a740.firebaseapp.com",
+  projectId: "assignment9-6a740",
+  storageBucket: "assignment9-6a740.appspot.com",
+  messagingSenderId: "717653557213",
+  appId: "1:717653557213:web:7ed4fdc49a49403bd34034"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export default app;
--- src/pages/Footer/Footer.jsx
+++ src/pages/Footer/Footer.jsx
@@ -1,8 +1,8 @@
 
 const Footer = () => {
     return (
-        <div>
-            <footer className="dark:bg-gray-900 m-4">
+        <div className="bg-white">
+            <footer className="m-4">
                 <div className="w-full max-w-screen-xl mx-auto p-4 md:py-8">
                     <div className="sm:flex sm:items-center sm:justify-between">
                         <a href="https://flowbite.com/" className="flex items-center mb-4 sm:mb-0 space-x-3 rtl:space-x-reverse">
